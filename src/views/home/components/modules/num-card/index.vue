21ef39055c08a9e568b7a10da2ae4e249a9109be
JonSnow <1571298579@qq.com>
2022-07-07 22:13:53 +0800

    no message

---
+++ src/views/home/components/modules/num-card/index.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div class="">121212</div>
+</template>
+
+<script>
+export default {
+  name: "NumCard",
+  components: {},
+  mixins: [],
+  props: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
